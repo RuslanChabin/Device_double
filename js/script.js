96260f598530ee007b5dc67a4d96408680b1e140
RuslanChabin <pycc_lan@mail.ru>
2018-07-24 00:51:03 +0300

    БЭМ

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-var popupBtn = document.querySelector(".btn_write");
-var popup = document.querySelector(".modal_write_us");
-var closeBtn = popup.querySelector(".btn_modal_close");
-var mapMini = document.querySelector(".map_img");
-var mapPopup = document.querySelector(".modal_map");
-var closeBtnMap = mapPopup.querySelector(".btn_map_close");
-var form = popup.querySelector(".modal_form");
+var popupBtn = document.querySelector(".button-write");
+var popup = document.querySelector(".modal-write-us");
+var closeBtn = popup.querySelector(".button-modal-close");
+var mapMini = document.querySelector(".device-information__map-img");
+var mapPopup = document.querySelector(".modal-map");
+var closeBtnMap = mapPopup.querySelector(".button-map-close");
+var form = popup.querySelector(".modal-form");
 var userName = popup.querySelector("[name=name]");
 var userMail = popup.querySelector("[name=email]");
 var letter = popup.querySelector("[name=message]");
@@ -19,7 +19,7 @@ try {
 
 popupBtn.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popup.classList.add("modal_show");
+  popup.classList.add("modal-show");
   if (storage) {
       userName.value = storage;
       userMail.focus();
@@ -30,18 +30,18 @@ popupBtn.addEventListener("click", function (evt) {
 
 closeBtn.addEventListener("click", function (evt){
   evt.preventDefault();
-  popup.classList.remove("modal_show");
-  popup.classList.remove("modal_error");
+  popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
 });
 
 mapMini.addEventListener("click", function (evt){
   evt.preventDefault();
-  mapPopup.classList.add("modal_show");
+  mapPopup.classList.add("modal-show");
 });
 
 closeBtnMap.addEventListener("click", function (evt){
   evt.preventDefault();
-  mapPopup.classList.remove("modal_show");
+  mapPopup.classList.remove("modal-show");
 });
 
 
@@ -53,20 +53,20 @@ form.addEventListener("submit", function (evt){
     localStorage.setItem("letter", letter.value);
     } else {
       evt.preventDefault();
-      popup.classList.remove("modal_error");
+      popup.classList.remove("modal-error");
       popup.offsetWidth = popup.offsetWidth;
-      popup.classList.add("modal_error");
+      popup.classList.add("modal-error");
     }
 });
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
-    if (popup.classList.contains("modal_show")) {
-      popup.classList.remove("modal_show");
-      popup.classList.remove("modal_error");
-    } if (mapPopup.classList.contains("modal_show")) {
-      mapPopup.classList.remove("modal_show")
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
+    } if (mapPopup.classList.contains("modal-show")) {
+      mapPopup.classList.remove("modal-show")
     }
   }
 });
